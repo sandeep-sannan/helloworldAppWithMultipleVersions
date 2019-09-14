09e868771645a06533eff9bd763578ea7b7f89fe
Sandeep K S <Sandeep.KS@gds.ey.com>
2019-09-14 16:30:30 +0530

    added controller to hello-message-app at 8888

--- spring-hello-world-gcp/src/main/java/com/example/springhelloworldgcp/HelloWorldController.java
+++ spring-hello-world-gcp/src/main/java/com/example/springhelloworldgcp/HelloWorldController.java
@@ -1,21 +1,34 @@
 package com.example.springhelloworldgcp;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class HelloWorldController {
+    
+        @Autowired
+        RestTemplate restTemplate;
 
-    @GetMapping("/")
-    public ResponseEntity<String> getMessage() {
-        return ResponseEntity.ok("Pod Is Up");
-    }
+        @RequestMapping("/welcome")
+        public String welcome() {
+            String message ="";
+            try {
+                message = restTemplate.getForObject("http://hello-message-app:8888/hello", String.class);
+            }catch(Exception e){
+                System.out.println("Exception " + e.getMessage());
+            }
+            return message;
+        }
 
-    @GetMapping("/version")
-    public ResponseEntity<String> getMessageWithVersion() {
-        return ResponseEntity.ok("Ver 0");
-    }
+        @Bean
+        public RestTemplate rest() {
+            return new RestTemplate();
+        }
+
 }
