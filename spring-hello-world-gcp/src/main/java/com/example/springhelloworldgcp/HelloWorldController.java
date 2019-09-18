62af9ffb18c2bb5ff76f86c0a359d39bbb507d1c
Sandeep K S <Sandeep.KS@gds.ey.com>
2019-09-18 14:31:06 +0530

    rest call azure pod created

--- spring-hello-world-gcp/src/main/java/com/example/springhelloworldgcp/HelloWorldController.java
+++ spring-hello-world-gcp/src/main/java/com/example/springhelloworldgcp/HelloWorldController.java
@@ -1,16 +1,43 @@
 package com.example.springhelloworldgcp;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class HelloWorldController {
 
-    @RequestMapping("/hello")
+    @RequestMapping("/hello-old")
     public String hello() {
         return String.format("This is hello from version 2");
     }
+
+    @Autowired
+    RestTemplate restTemplate;
+
+    @RequestMapping("/hello")
+    public String welcome() {
+        System.out.println("Hello End Point Called...");
+        String message ="This is hello from version 2 + [ ";
+        String azMsg="";
+        try {
+            azMsg = restTemplate.getForObject("http://azure-hello-message-app:8080/welcome", String.class);
+        }catch(Exception e){
+            System.out.println("Exception " + e.getMessage());
+            azMsg = "failed to call azure pods";
+        }
+        message= message+azMsg +" ]";
+        return message;
+    }
+
+    @Bean
+    public RestTemplate rest() {
+        return new RestTemplate();
+    }
+
 }
