e21905b8ccc4337943702f9e7bf8d055fd464adf
arun-kantha <arun.kantha@gds.ey.com>
2019-09-20 13:00:59 +0530

    Updated hello message service name

--- spring-hello-world-gcp/src/main/java/com/example/springhelloworldgcp/HelloWorldController.java
+++ spring-hello-world-gcp/src/main/java/com/example/springhelloworldgcp/HelloWorldController.java
@@ -20,7 +20,7 @@ public class HelloWorldController {
             System.out.println("Welcome End Point Called...GCP Master");
             String message ="";
             try {
-                message = restTemplate.getForObject("http://gcp-hello-message-app:8888/hello", String.class);
+                message = restTemplate.getForObject("http://hello-message-app:8888/hello", String.class);
             }catch(Exception e){
                 System.out.println("Exception " + e.getMessage());
             }
